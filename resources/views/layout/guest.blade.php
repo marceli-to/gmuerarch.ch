9187107b4d6af737d5ecd20ecd94e7bb1eeb5d67
Marcel Stadelmann <m@marceli.to>
2022-11-29 16:47:57 +0100

    updates project list

--- resources/views/layout/guest.blade.php
+++ resources/views/layout/guest.blade.php
@@ -1,6 +1,6 @@
 @include('layout.partials.head')
 <body class="auth">
-<main role="main" class="site">
+<main role="main" class="site site--auth">
   <div>
     <section class="mx-auto">
       @include('icons.logo')
