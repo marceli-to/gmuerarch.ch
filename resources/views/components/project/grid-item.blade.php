c9300e54e211c908914d7c6dde464addc92b6d00
Marcel Stadelmann <m@marceli.to>
2022-12-02 10:39:07 +0100

    bugfix

--- resources/views/components/project/grid-item.blade.php
+++ resources/views/components/project/grid-item.blade.php
@@ -1,16 +1,18 @@
-<figure class="{{ !$stack ? 'content-grid__item' : ''}}">
-  <a href="{{ route('page.project.show', 
-      [
-        'category' => $gridItem->project->categories->first()->slug, 
-        'slug' => AppHelper::slug($gridItem->project->title), 
-        'project' => $gridItem->image->imageable_id
-      ]
-    )}}">
-    <x-image 
-      :maxSizes="[1200 => 1500, 900 => 1200, 0 => 900]" 
-      width="1600"
-      height="900"
-      :image="$gridItem->image" 
-    />
-  </a>
-</figure>
+@if ($gridItem)
+  <figure class="{{ !$stack ? 'content-grid__item' : '' }}">
+    <a href="{{ route('page.project.show', 
+        [
+          'category' => $gridItem->project->categories->first()->slug, 
+          'slug' => AppHelper::slug($gridItem->project->title), 
+          'project' => $gridItem->image->imageable_id
+        ]
+      )}}">
+      <x-image 
+        :maxSizes="[1200 => 1500, 900 => 1200, 0 => 900]" 
+        width="1600"
+        height="900"
+        :image="$gridItem->image" 
+      />
+    </a>
+  </figure>
+@endif
